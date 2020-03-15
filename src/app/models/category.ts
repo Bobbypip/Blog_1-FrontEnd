bb78acf0f14a10ace552f12b814c44ed57a998e3
Bobbypip <51970655+Bobbypip@users.noreply.github.com>
2020-03-14 23:41:36 -0600

    Main start

---
+++ src/app/models/category.ts
@@ -0,0 +1,8 @@
+export class Category{
+    //Agregar export a la clase para poder utilizarla despues en otros ficheros
+    constructor(
+        public id: number,
+        public name: string,
+    ){
+    }
+}
---
+++ src/app/models/post.ts
@@ -0,0 +1,13 @@
+export class Post{
+    //Agregar export a la clase para poder utilizarla despues en otros ficheros
+    constructor(
+        public id: number,
+        public user_id: string,
+        public category_id: number,
+        public title:string,
+        public content: string,
+        public image: string,
+        public createdAt: any
+    ){
+    }
+}
