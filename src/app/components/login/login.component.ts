544ad98ce6c0e0edfa64f18f2395190b24229651
Bobbypip <51970655+Bobbypip@users.noreply.github.com>
2020-04-03 21:27:27 -0600

    Initial Commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService} from '../../services/user.service';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
   public page_title: string;
+  public user: User;
+  public status: string;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.page_title = 'Identificate';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
    }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+    this._userService.signup(this.user).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    )
   }
 
 }
